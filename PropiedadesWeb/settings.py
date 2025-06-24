e690b6db632b48ecb665739b47589df64e6e8126
jesrojas98 <jes.rojasg@duocuc.cl>
2025-06-24 16:36:55 -0400

    Initial commit for Railway deployment 1

--- PropiedadesWeb/settings.py
+++ PropiedadesWeb/settings.py
@@ -162,7 +162,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='godoykepcl@gmail.com')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='jesusalerojasguti@gmail.com')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='ypdn xupf pllz abrd')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
